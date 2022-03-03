b97ba63e4416f6c4539cfca513bb9ff974f14079
Joan Comas <joan.comas.fdz@gmail.com>
2022-03-03 22:42:31 +0100

    Added my name in the packages, cause everybody does this

---
+++ ExampleApplicationWithConsole.Tests/GeneratedScenarios/JoanComas.ScenarioUnitTesting.SourceGenerator/ScenarioUnitTesting.SourceGenerator.ScenarioGenerator/WebScrapperScenario.g.cs
@@ -0,0 +1,11 @@
+﻿// Auto-generated code with ScenarioUnitTesting.SourceGenerator at 03/03/2022 22:42:01
+
+using ScenarioUnitTesting;
+
+namespace ExampleApplicationWithConsole.Tests
+{
+    public class WebScrapperScenario : Scenario<ExampleApplicationWithConsole.Domain.WebScrapper>
+    {
+        public ExampleApplicationWithConsole.Domain.WebScrapper WebScrapper { get { return base.When(); } }
+    }
+}
