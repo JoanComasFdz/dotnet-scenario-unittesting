e3c44e40f828a7179e466ddab8e37f5e386a93b3
Joan Comas <joan.comas.fdz@gmail.com>
2022-10-03 11:30:03 +0200

    Improved Scenario class documentation

--- JoanComas.ScenarioUnitTesting/Scenario.cs
+++ JoanComas.ScenarioUnitTesting/Scenario.cs
@@ -7,6 +7,27 @@ using AutoFixture;
 
 namespace JoanComas.ScenarioUnitTesting
 {
+    /// <summary>
+    /// This class provides all tools necessary to test a class, which include:
+    /// <list type="bullet">
+    ///     <item>
+    ///         <description>
+    ///             Automatically mocks all the system under test's dependencies, creating substitutes for all injected parameters from all its constructors.
+    ///         </description>
+    ///     </item>
+    ///     <item>
+    ///         <description>
+    ///             Automatically instantiates the system under test with the mocked dependencies via the <c>When()</c> method.
+    ///         </description>
+    ///     </item>
+    ///     <item>
+    ///         <description>
+    ///             Single access point for all dependencies via the <c>Dependency&lt;T&gt;()</c> method.
+    ///         </description>
+    ///     </item>
+    /// </list>
+    /// </summary>
+    /// <typeparam name="TSut">The type of the system under test.</typeparam>
     public class Scenario<TSut> where TSut : class
     {
         protected IFixture Fixture { get; }
@@ -15,10 +36,10 @@ namespace JoanComas.ScenarioUnitTesting
         {
             Fixture = new Fixture();
 
-            // The trick is to create the mocks for all parameters in advance
-            // and register them in the Fixture.
-            // This way, a test may call the When() first and then Dependency<T>() to assert without configuring
-            // and it will still work.
+            // The approach is to create the mocks for all parameters in advance
+            // and inject them in the Fixture.
+            // This way, a test may call the When() first and then Dependency<T>()
+            // to assert without configuring and it will still work.
             GetAllParametersFromAllConstructors()
                 .Select(parameter => new
                 {
@@ -41,12 +62,21 @@ namespace JoanComas.ScenarioUnitTesting
             return parameters1;
         }
 
+        /// <summary>
+        /// Returns the dependency of the specified type.
+        /// </summary>
+        /// <typeparam name="TDependency">The type of dependency to be returned.</typeparam>
+        /// <returns>The existing instance of the substitute for the specified dependency type.</returns>
         public TDependency Dependency<TDependency>() where TDependency : class
         {
             var substitute = Fixture.Create<TDependency>();
             return substitute;
         }
 
+        /// <summary>
+        /// Returns the system under test.
+        /// </summary>
+        /// <returns>The instance of the system under test with all its dependencies mocked.</returns>
         public TSut When()
         {
             var instance= Fixture.Create<TSut>();
